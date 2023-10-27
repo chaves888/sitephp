43598295c5eb7ee028aa8ee7699f3f55db29df69
Leonardo Chaves Batista da Silva <127910384+chaves888@users.noreply.github.com>
2023-10-26 22:22:32 -0300

    Add files via upload

---
+++ esqueleto-site-main/dados.php
@@ -0,0 +1,35 @@
+<?php
+
+    $produtos[1] = array("id"=>1,
+        "nome"=>"Glock-18 | Bullet Queen",
+        "valor"=>"2.600,90",
+        "imagem"=>"01.webp");
+    $produtos[2] = array("id"=>2,
+        "nome"=>"AK-47 | Wild Lotus",
+        "valor"=>"1.900,00",
+        "imagem"=>"02.webp");
+    $produtos[3] = array("id"=>3,
+        "nome"=>"AK-47 | Hoad Shot",
+        "valor"=>"2300,90",
+        "imagem"=>"03.webp");
+    $produtos[4] = array("id"=>4,
+        "nome"=>"AWP | Doodle Lore",
+        "valor"=>"4.490,99",
+        "imagem"=>"04.webp");
+    $produtos[5] = array("id"=>5,
+        "nome"=>"AWP | Gungir",
+        "valor"=>"2000,50",
+        "imagem"=>"05.webp");
+    $produtos[6] = array("id"=>6,
+        "nome"=>"AK-47 | Wild Fire",
+        "valor"=>"3600,90",
+        "imagem"=>"06.webp");
+    $produtos[7] = array("id"=>7,
+        "nome"=>"M4A1-S Boost Protocol",
+        "valor"=>"1500,90",
+        "imagem"=>"07.webp");     
+    $produtos[8] = array("id"=>8,
+        "nome"=>"M4A4 | Temukau",
+        "valor"=>"100,90",
+        "imagem"=>"08.webp");                                     
+        
